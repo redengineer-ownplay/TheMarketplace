452b6c59291ce348dea954c5d643bef41857a435
Red <red.pace.dev@gmail.com>
2025-02-23 19:36:17 +0100

    feat: Added common ui components

---
+++ frontend/src/components/ui/SafeImage.tsx
@@ -0,0 +1,25 @@
+import Image, { ImageProps } from 'next/image';
+import { useState } from 'react';
+
+const SafeImage = ({ src, alt, ...props }: ImageProps) => {
+  const [error, setError] = useState(false);
+
+  if (typeof src === 'string' && src.startsWith('ipfs://')) {
+    src = `https://ipfs.io/ipfs/${src.replace('ipfs://', '')}`;
+  }
+
+  if (error) {
+    return <div>Image failed to load</div>;
+  }
+
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      onError={() => setError(true)}
+      {...props}
+    />
+  );
+};
+
+export default SafeImage;
